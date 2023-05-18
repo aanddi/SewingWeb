f504d33b38e427befccf422eaeaf53b8e41c66cc
aanddi <kuropatkinn.a.i21@gmail.com>
2023-05-19 01:37:10 +0300

    Создание структуры веб-приложения

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './styles/App.scss'
 
 function App() {
   return (
     <div className="App">
-    <h1>SewingWeb</h1>
+    <h1 className='title'>SewingWeb</h1>
     </div>
   );
 }
