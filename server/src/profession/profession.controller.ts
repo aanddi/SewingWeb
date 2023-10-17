bff9d8297398b1444c3b2aeb0d8a5bac98af8355
aanddi <kuropatkinn.a.i21@gmail.com>
2023-10-17 12:57:49 +0300

    Разработка сервиса профессий

--- server/src/profession/profession.controller.ts
+++ server/src/profession/profession.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Query, ValidationPipe, UsePipes } from '@nestjs/common'
 import { ProfessionService } from './profession.service'
+
 
 @Controller('profession')
 export class ProfessionController {
   constructor(private readonly professionService: ProfessionService) {}
 
-  @Get()
+  @Get('all')
   getAllProfession() {
-    return this.professionService.getAll()
+    return this.professionService.getAllProfession()
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Get()
+  getSortBySalary(@Query('sort') sort: string) {
+    return this.professionService.getSortBySalary(sort)
   }
 }
--- server/src/profession/profession.service.ts
+++ server/src/profession/profession.service.ts
@@ -1,11 +1,43 @@
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from 'src/prisma.service'
+import { Profession } from '@prisma/client'
 
 @Injectable()
 export class ProfessionService {
   constructor(private prisma: PrismaService) {}
 
-  getAll() {
-    return this.prisma.profession.findMany()
+  async getAllProfession() {
+    const professions = await this.prisma.profession.findMany({
+      select: {
+        name: true,
+        averageSalary: true,
+        desc: true
+      }
+    })
+
+    return professions
+  }
+
+  async getSortBySalary(sort) {
+    const professions = await this.prisma.profession.findMany({
+      orderBy: {
+        averageSalary: sort
+      },
+      select: {
+        name: true,
+        averageSalary: true,
+        desc: true
+      }
+    })
+
+    return professions
+  }
+
+  private returnProfessionsFields(profession: Profession) {
+    return [{
+      name: profession.name,
+      averageSalary: profession.averageSalary,
+      desc: profession.desc,
+    }]
   }
 }
