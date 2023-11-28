1add122a6f7d8b0c9f2cf7b3183d300430887714
aanddi <kuropatkinn.a.i21@gmail.com>
2023-11-28 23:38:47 +0300

    Попытка деплоя

--- server/src/prisma.service.ts
+++ server/src/prisma.service.ts
@@ -1,9 +1,9 @@
-import { Injectable, OnModuleInit } from '@nestjs/common'
-import { PrismaClient } from '@prisma/client'
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super();
   }
 }
