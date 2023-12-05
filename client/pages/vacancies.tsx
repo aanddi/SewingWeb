230ec262e781b3e4a7d7b89daf52d3bc7db624c7
aanddi <kuropatkinn.a.i21@gmail.com>
2023-12-06 02:09:29 +0300

    build

---
+++ client/pages/vacancies.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next'
+
+import Meta from '@/components/Meta/Meta'
+
+const VacanciesListPage: NextPage = () => {
+  return (
+    <Meta title="Вакансии">
+      <>Вакансии</>
+    </Meta>
+  )
+}
+
+export default VacanciesListPage
